024626e392508f8bb78487795ecc97081dc97c2f
jeffpal <jeffersonfpalheta@gmail.com>
2019-05-30 17:32:50 -0300

    update

--- snippets/socket.io-server/client.js
+++ snippets/socket.io-server/client.js
@@ -1,4 +1,10 @@
-//CLIENT TEST
+/* 
+    © 2019 Jeff Pal.  All rights reserved.
+    Project: Socket.io Server
+    Author: Jeff Pal
+    Git repository: https://github.com/jeff-pal
+    Helpfull reference: https://gist.github.com/luciopaiva/e6f60bd6e156714f0c5505c2be8e06d8
+ */
 
 const
     io = require("socket.io-client"),
--- snippets/socket.io-server/server.js
+++ snippets/socket.io-server/server.js
@@ -1,13 +1,21 @@
+/*
+    © 2019 Jeff Pal.  All rights reserved.
+    Project: Socket.io Server
+    Author: Jeff Pal
+    Git repository: https://github.com/jeff-pal
+    Helpfull reference: https://gist.github.com/luciopaiva/e6f60bd6e156714f0c5505c2be8e06d8
+*/
+
 const
     io = require("socket.io"),
     server = io.listen(8000);
 
+var x, y;
 
 // event fired every time a new client connects:
 server.on("connection", (socket) => {
     console.info(`Client connected [id=${socket.id}]`);
 
-    // when socket disconnects, remove it from the list:
     socket.on("disconnect", () => {
         console.info(`Client gone [id=${socket.id}]`);
     });
@@ -24,13 +32,11 @@ server.on("connection", (socket) => {
         console.info(`Msg: ${msg}`);
         socket.emit("reply", "pong");
     });
+
+    socket.on("getData", () => {
+        x = (new Date()).getTime()
+        y = Math.random();
+        socket.emit("response", [x, y]);
+        console.log(`Data sent: ${[x, y]}`);
+    });
 });
-
-
-// // sends each client its current sequence number
-// setInterval(() => {
-//     for (const [client, sequenceNumber] of sequenceNumberByClient.entries()) {
-//         client.emit("seq-num", sequenceNumber);
-//         sequenceNumberByClient.set(client, sequenceNumber + 1);
-//     }
-// }, 1000);
